7bcf4f903ae63c365076e293d3fec07d9c62c4d3
Mike Rachman <mikerachman1@gmail.com>
2023-01-12 10:54:13 -0500

    added Cards component and flags folder of imgs

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="main">
+      <div className="card">
+
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
