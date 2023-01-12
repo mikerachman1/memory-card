90facd5c10e82524238672b7ad945af0707e8e91
Mike Rachman <mikerachman1@gmail.com>
2023-01-12 14:17:58 -0500

    handleClick logic completed, added useEffect hooks for localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,50 @@ import Card from "./Card";
 // have button that toggles dispalying flag names
 
 function App() {
-  const [cards, setCards] = useState(getRandomFlags())
+  const [cards, setCards] = useState(getRandomFlags());
+  const [clicked, setClicked] = useState([]);
   const [current, setCurrent] = useState(0);
   const [high, setHigh] = useState(0);
+
+  const resetGame = () => {
+    setCurrent(0);
+    setClicked([]);
+    setCards(getRandomFlags());
+  }
+
+  const handleClick = (flagId) => {
+    if (clicked.includes(flagId)) {
+      return resetGame();
+    }
+    setClicked(clicked.concat(flagId));
+    setCards(getRandomFlags());
+    setCurrent(current + 1);
+    if (current >= high) { setHigh(high + 1) }
+  }
+
+  //(ON UPDATE) if new high score write to local storage
+  useEffect(() => {
+    if (high === 0) {return}
+    localStorage.setItem('High', high);
+  }, [high]);
+
+  //(ON MOUNT) get high score from storage if exists
+  useEffect(() => {
+    const savedHighScore = parseInt(localStorage.getItem('High'));
+    if (savedHighScore) { setHigh(savedHighScore) }
+  }, []);
 
   return (
     <div>
       <Header current={current} high={high}/>
-      <div>
+      <div className="card-container">
         {cards.map((card) => {
-          return <Card src={card.src} name={card.name}/>
+          return <Card 
+                  key={card.id}
+                  id ={card.id}
+                  src={card.src}
+                  handleClick={handleClick}
+                  />
         })}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="card">
+    <div className="card" onClick={() => props.handleClick(props.id)}>
       <img src={props.src} alt={props.name}/>
     </div>
   );
